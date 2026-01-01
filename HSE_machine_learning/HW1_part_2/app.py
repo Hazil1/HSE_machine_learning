21ff8a2d699fda666b6dcd4975efc80c9018dd7e
Ilshat Khaziev <irkhaziev@edu.hse.ru>
2026-01-01 21:59:59 +0300

    solution part2

--- HSE_machine_learning/HW1_part_2/app.py
+++ HSE_machine_learning/HW1_part_2/app.py
@@ -10,7 +10,35 @@ from sklearn.linear_model import Ridge
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 
-from utils import split_torque
+
+def split_torque(torque):
+    '''
+    Функция делит столбец torque на два столбца (torque, max_torque_rpm), преобразует
+    крутящий момент из 'kgm' в 'Nm' и избавляется от единиц измерения в столбце torque
+    '''
+    if not pd.isna(torque):
+        try:
+            if '@' in torque:
+                parts = torque.split('@', 1)
+                torque = parts[0].strip()
+                max_torque_rpm = parts[1].strip()
+            elif 'at' in torque:
+                parts = torque.split('at', 1)
+                torque = parts[0].strip()
+                max_torque_rpm = parts[1].strip()
+
+            # Преобразуем `kgm` в `Nm` и избавимся от единиц измерения
+            if 'kgm' in torque.lower() or 'kgm' in max_torque_rpm.lower():
+                torque = float(re.sub(r'[^\d.]', '', torque)) * 9.8
+            elif 'nm' in torque.lower():
+                torque = float(re.sub(r'[^\d.]', '', torque))
+            else:
+                torque = float(torque)
+            return torque
+        except:
+            return np.nan
+    else:
+        return np.nan
 
 
 st.title("Оценка стоимости автомобиля с помощью линейной регрессии")
